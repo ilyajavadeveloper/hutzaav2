fc9dcf0b01959475049fb978150bd6cbe2922f1a
Ilya Hvostenko <ilyajavadev@gmail.com>
2026-03-23 20:06:32 +0200

    style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ContactSection from "./components/ContactSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/NavBar"
 import PaintingSection from './components/PaintingSection'
-// App.jsx
 import "./App.css";
 import HomeSection from "./components/HomeSection"
 
